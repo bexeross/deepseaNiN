4c54d264729aa162c4e7ff31ed16c91d1e5c881b
bexeross <bexeross@googlemail.com>
2022-09-14 13:40:37 +0200

    amend save location

--- Config.R
+++ Config.R
@@ -11,11 +11,13 @@ userName <- "C:/Users/a21448/" # bex's path
 #userName <- "C:/Users/genoveva/"
   
   
-dataPath1 <- "ownCloud/NiN_Mareano/inputs"
+
+dataPath1 <- "OneDrive - Havforskningsinstituttet/General/inputs"
 dataPath2 <- "Havforskningsinstituttet/Video analysis lab - Reference List"
 
 dataPath <- paste0(userName, dataPath1)
 dataPath_2 <- paste0(userName, dataPath2)
+
 
 #sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
 #refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
