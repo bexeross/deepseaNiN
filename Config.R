6c456fed23c9bb4037381809ee894f31af393082
bexeross <bexeross@googlemail.com>
2022-09-14 09:44:44 +0200

    commented currently unused to avoid cluttering env

--- Config.R
+++ Config.R
@@ -17,21 +17,21 @@ dataPath2 <- "Havforskningsinstituttet/Video analysis lab - Reference List"
 dataPath <- paste0(userName, dataPath1)
 dataPath_2 <- paste0(userName, dataPath2)
 
-sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
-refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
-sample_info <- read.csv(file.path(dataPath,"sample_info.csv"))
+#sppdens <- read.csv(file.path(dataPath,"species_densities.csv"))
+#refer <- read.table(file.path(dataPath,"reference.csv"), sep ="|", header = TRUE)
+#sample_info <- read.csv(file.path(dataPath,"sample_info.csv"))
 
-taxonary <- read_xlsx(file.path(dataPath_2, "Taxonary.xlsx"), sheet=1)
+#taxonary <- read_xlsx(file.path(dataPath_2, "Taxonary.xlsx"), sheet=1)
 
 
 ## Some functions
 
 #convert geographic coordinates in WGS84 to UTM33N
 
-utmize <- function(x){
-  require(rgdal)
-  proj4string(x)=CRS("+init=epsg:4326")
-  result <- spTransform(x, CRS("+init=epsg:32633")) 
-  return(result)
-}
+#utmize <- function(x){
+#   require(rgdal)
+#   proj4string(x)=CRS("+init=epsg:4326")
+#   result <- spTransform(x, CRS("+init=epsg:32633")) 
+#   return(result)
+# }
 
