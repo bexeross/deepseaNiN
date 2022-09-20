9e7d1e8d5d98d1ecdf05b47a6428cda27295322f
bexeross <bexeross@googlemail.com>
2022-09-20 12:30:24 +0200

    remove slash

--- Config.R
+++ Config.R
@@ -6,8 +6,8 @@ library(readxl)
 
 # C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
 
-#datapath <- "C:/Users/genoveva/OneDrive - Havforskningsinstituttet/General/" # geno's path
-datapath<- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General/" # bex's path
+#datapath <- "C:/Users/genoveva/OneDrive - Havforskningsinstituttet/General" # geno's path
+dataPath<- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General" # bex's path
 #datapath <- "C:/Users/Wilson_Margaret/Havforskningsinstituttet/NiN + MAREANO - General" # Margaret's path
 
 
