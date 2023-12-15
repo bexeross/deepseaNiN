cdd94ce8a501a37d8200f651495a001c4920ffb5
bexeross <bexeross@googlemail.com>
2023-12-15 10:12:48 +0100

    update paths

--- Config.R
+++ Config.R
@@ -1,7 +1,7 @@
 
 ## Paths
 
-library(readxl)
+#library(readxl)
 # https://owncloud.imr.no/index.php/s/lCkOERjzJkPItZj
 
 # C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
@@ -10,4 +10,4 @@ library(readxl)
 dataPath<- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General" # bex's path
 #datapath <- "C:/Users/Wilson_Margaret/Havforskningsinstituttet/NiN + MAREANO - General" # Margaret's path
 
-
+dataPath_2 <- "C:/Users/a21448/Havforskningsinstituttet/Video analysis lab - Reference List"
